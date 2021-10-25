cd5e70a2a0cf916e45d879cd742da222faa2d248
kacperpalis <kacperpalis@gmail.com>
2021-10-25 10:50:13 +0200

    test

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,4 +1,22 @@
 //
 // Created by Kacper on 2021-10-18.
 //
+#include <string>
 
+class PrzykladowaKlasa
+{
+public:
+    double liczba; //prawo dostępu: publiczne
+    char tablica[20]; //prawo dostępu: publiczne
+
+private:
+    int abc; //prawo dostępu: prytatne
+    char znak; //prawo dostępu: prytatne
+    std::string napis; //prawo dostępu: prytatne
+};
+
+int main()
+{
+    PrzykladowaKlasa nazwaZmiennej;
+    return(0);
+}
